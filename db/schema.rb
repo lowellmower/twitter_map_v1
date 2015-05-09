e4324a962cea005c49d4ff3446e481b4a4df9ac0
Lowell Mower <lowell.m.mower@gmail.com>
2015-05-09 12:19:33 -0400

    set up user authenticaiton with oauth gem and twitter gem for REST requests

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509140458) do
+ActiveRecord::Schema.define(version: 20150509155953) do
 
   create_table "tweets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "token"
+    t.string   "secret"
+    t.string   "profile_image"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
